d47ae6557b1340e8a75732ad919cd3f08f7108e1
eticialima <contato.leticialima.me@gmail.com>
2021-09-06 01:39:31 -0300

    atualizado camera

--- src/app/shadow/shadow.component.ts
+++ src/app/shadow/shadow.component.ts
@@ -28,78 +28,42 @@ export class ShadowComponent implements OnInit {
   ngOnInit(): void {
     this.main();
   }
-
-  addPlane(scene) {
-    const planeSize = 10;
-
-    const loader = new THREE.TextureLoader();
-    const texture = loader.load('https://threejsfundamentals.org/threejs/resources/images/checker.png');
-    texture.wrapS = THREE.RepeatWrapping;
-    texture.wrapT = THREE.RepeatWrapping;
-    texture.anisotropy = 4;
-    texture.magFilter = THREE.NearestFilter;
-    texture.encoding = THREE.sRGBEncoding;
-    const repeats = planeSize / 2;
-    texture.repeat.set(repeats, repeats);
-
-    const planeGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize);
-    const planeMat = new THREE.MeshPhongMaterial({
-      map: texture,
-      side: THREE.DoubleSide,
-    });
-    const mesh = new THREE.Mesh(planeGeo, planeMat);
-    mesh.receiveShadow = true;
-    mesh.rotation.x = Math.PI * -0.5; 
-    mesh.position.y = 0; 
-    scene.add(mesh);
-    this.reRender();
-  }
-
+  
   reRender() {
     const event = new Event('resize');
     setTimeout(() => {
       dispatchEvent(event);
     });
   }
-
-
-
+ 
   main() {
-    const gltfLoader = new GLTFLoader()  
-    //const gui = new dat.GUI() 
+    const gltfLoader = new GLTFLoader()   
     const canvas: any = document.querySelector('#canvas'); 
+    const container = document.getElementById( 'canvas' );
     const scene = new THREE.Scene(); 
-    let tl = gsap.timeline() 
-    //this.addPlane(scene); 
-
-    let spotLight, lightHelper, shadowCameraHelper;
-    let lightProbe;
+    let tl = gsap.timeline()  
 	  let directionalLight;
-			// linear color space
-	  const API = {
-				lightProbeIntensity: 1.0,
-				directionalLightIntensity: 0.2,
-				envMapIntensity: 1
-			}; 
 
 
+    //Camera 
+    const camera = new THREE.PerspectiveCamera( 25, window.innerWidth / window.innerHeight, 0.1, 100 );
+    camera.rotation.y = 45/180*Math.PI;
+    camera.position.x = -1;
+    camera.position.z = 4;
+    camera.position.y = 2;
+    scene.add(camera)
 
-    //3d  
-    gltfLoader.load('../../assets/aranha4.gltf', (gltf) => {
 
+    //Scene 
+    gltfLoader.load('../../assets/aranha4.gltf', (gltf) => { 
       gltf.scene.scale.set(0.3, 0.3, 0.3)
       gltf.scene.rotation.set(0, 1.1, 0)
-      gltf.scene.children[0]; 
-      
-      scene.add(gltf.scene)
+      gltf.scene.position.set(0, 1, 0)
+      scene.add(gltf.scene) 
+    }) 
 
-      //gui.add(gltf.scene.rotation, 'x').min(0).max(9)
-      //gui.add(gltf.scene.rotation, 'y').min(0).max(9)
-      //gui.add(gltf.scene.rotation, 'z').min(0).max(9) 
-    })
-    // SCENE
-    
 
+    // Lights
     const hlight = new THREE.AmbientLight (0x404040,1);
     scene.add(hlight);
 
@@ -122,167 +86,45 @@ export class ShadowComponent implements OnInit {
 
     const light4 = new THREE.PointLight(0xc4c4c4,1);
     light4.position.set(-1,1,1);
-    scene.add(light4);  
+    scene.add(light4);   
 
-
-
-    /*
-    scene.background = new THREE.Color( 0x000000 );
-    scene.fog = new THREE.Fog( 0x000000, 250, 1400 );
-    
-    const ambientLight = new AmbientLight(0x323232);
-		const mainLight = new DirectionalLight(0xffffff, 1.0);
-		mainLight.position.set(-1, 1, 1);
-
-		scene.add(ambientLight, mainLight);
-    // LIGHTS
-    
-    const dirLight = new THREE.DirectionalLight( 0xffffff, 0.125 );
-    dirLight.position.set( 0, 0, 1 ).normalize();
-    scene.add( dirLight );
-  
-    const spotLight = new THREE.SpotLight( 0xffffff );
-    spotLight.position.set( 100, 1000, 100 );
-
-    spotLight.castShadow = true;
-
-    spotLight.shadow.mapSize.width = 1024;
-    spotLight.shadow.mapSize.height = 1024;
-
-    spotLight.shadow.camera.near = 500;
-    spotLight.shadow.camera.far = 4000;
-    spotLight.shadow.camera.fov = 30;
-
-    scene.add( spotLight );
-    // Lights 
-    
-		const ambientLight = new AmbientLight(0x323232);
-		const mainLight = new DirectionalLight(0xffffff, 1.0);
-		mainLight.position.set(-1, 1, 1);
-
-		scene.add(ambientLight, mainLight);
-    
-    // Lights 
-     
-    const pointLight = new THREE.AmbientLight(0xffffff, .1)
-    pointLight.position.x = 2
-    pointLight.position.y = 3
-    pointLight.position.z = 4
-    scene.add(pointLight)
-  
-    const light1 = new THREE.DirectionalLight(0xffffff, 0.8);
-    light1.position.set(0, 10, 0);
-    
-    const light2 = new THREE.DirectionalLight(0xffffff, 1);
-    light2.castShadow = true;
-    light2.position.set(-5, 4, -5);
-
-    const light3 = new THREE.DirectionalLight(0xffffff, 0.8);
-    light3.position.set(10, 4, 10);
-
-    scene.add(light1);
-    scene.add(light2);
-    scene.add(light3);
-    */
-    const sizes = {
-      width: window.innerWidth,
-      height: window.innerHeight
-    }
-
-    window.addEventListener('resize', () => {
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-    })
-
-
-   
-    
-
-
-    /**
-   * Camera
-   */
-    // Base camera  
-    const camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 1, 1000 );
-    //const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-    camera.rotation.y = 45/180*Math.PI;
-    camera.position.x = -4;
-    camera.position.z = 4;
-    camera.position.y = 2;
-    scene.add(camera)
-  
-
-    // Controls Fixo
-    // const controls = new OrbitControls( camera, canvas );
-    // controls.maxPolarAngle = 0.9 * Math.PI / 2;
-    // controls.enableZoom = false;
-
-    // Controls Livre 
-    const controls = new OrbitControls(camera, canvas)
-    controls.enableDamping = true
-    controls.enableZoom = true
-    controls.enablePan = true  
-    controls.update(); 
-    
-    
-    /**
-     * Renderer
-     */  
-    const renderer = new THREE.WebGLRenderer({
-      canvas: canvas,
-      alpha: true,
-      antialias: true,
-      //powerPreference: "high-performance",
-      //antialias: false,
-      //stencil: false,
-      //depth: false
-    })
-    renderer.shadowMap.enabled = true;
-    //renderer.setSize(window.innerWidth, window.innerHeight) 
-    //renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)) 
-    //renderer.physicallyCorrectLights = true;
-    renderer.outputEncoding = THREE.sRGBEncoding; 
-    //renderer.toneMapping = THREE.ReinhardToneMapping;  
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap;  
-    //document.body.appendChild( canvas );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    const gridHelper = new THREE.GridHelper( 10, 20, 0x888888, 0x444444 );
+    gridHelper.position.set(0, 1.2, 0)
+    scene.add( gridHelper );
  
-   
-    /**
-   * Animate
-   */
+    //Renderer   
+    const renderer = new THREE.WebGLRenderer({  
+      antialias: true, 
+    })
+    renderer.setPixelRatio( window.devicePixelRatio );
+  	renderer.setSize( window.innerWidth, window.innerHeight ); 
+    container.appendChild( renderer.domElement );
+ 
+    // Controls Livre 
+    const controls = new OrbitControls( camera, renderer.domElement );
+    controls.screenSpacePanning = true;
+    controls.minDistance = 5;
+    controls.maxDistance = 40;
+    controls.target.set( 0, 2, 0 );
+    controls.update();
     
-    const clock = new THREE.Clock() 
-    const tick = () => { 
-        const elapsedTime = clock.getElapsedTime()
-  
-        // Update Orbital Controls
-        controls.update()
-
-        // Render
-        renderer.render(scene, camera)
-
-        // Call tick again on the next frame
-        window.requestAnimationFrame(tick)
+    window.addEventListener( 'resize', onWindowResize );
+    function onWindowResize() { 
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix(); 
+      renderer.setSize( window.innerWidth, window.innerHeight ); 
     }
 
-    tick() 
-       
-    function render() { 
-      lightHelper.update(); 
-      shadowCameraHelper.update(); 
-      renderer.render( scene, camera ); 
-    } 
+    const clock = new THREE.Clock() 
+        const tick = () => {    
+        controls.update() 
+        renderer.render(scene, camera) 
+        window.requestAnimationFrame(tick)
+       } 
+        tick() 
+        function render() {  
+          renderer.render( scene, camera ); 
+        }  
+        render()
   } 
 }
